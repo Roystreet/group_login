8c9a67086944ce2b0cb08f5a581c5ba44564d87a
jsilva <silvajonatanivan@gmail.com>
2021-08-08 15:50:37 -0300

    Estilos minimos en Home component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { Route } from 'react-router-dom'
 import { Login } from './component/login/Login'
 import { SignUp } from './component/singup/SignUp'
 import { Home } from './component/home/Home';
-// comentario
+
 export function App() {
   return (
     <>
       <Route exact path='/' component={Home} />
       <Route path="/login" component={Login} />
-      <Route path="/signup" render={() => <SignUp titulo='SignUp component 235' />} />
+      <Route path="/signup" component={SignUp} />
     </>
   )
 }
--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import s from './home.module.css'
 
 export function Home() {
     return (
-        <div>Hola</div>
+        <main className={s.main}>
+            <div className={s.container}>
+                <h1>Hola</h1>
+                <Link to='/login'>
+                    <button className={s.button}>Login</button>
+                </Link>
+                <Link to='/singup'>
+                    <button className={s.button}>Sing Up</button>
+                </Link>
+            </div>
+        </main>
     )
 }
